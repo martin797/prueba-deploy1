8c6ef565d20670c76008886561b5b05e6f626b39
Martin campoverde solorzano <alejandro.campoverde@ucuenca.edu.ec>
2020-07-22 20:30:23 -0500

    prueba

---
+++ Practica2.0/CrearProyecto/martin/src/main/java/com/example/martin/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.martin;
+
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
